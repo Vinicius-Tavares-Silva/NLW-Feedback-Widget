d10be4d72f19dd163dd95f34d3572d2fd81dd2b5
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-05-05 22:25:10 -0300

    Finish Backend development

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '840ed2174b0e43',
+    pass: '88e0f794745706',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Vinicius Tavares <vini_tavares10@hotmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
---
+++ server/src/use-cases/submit-feedback.ts
@@ -0,0 +1,35 @@
+import { MailAdapter } from '../adapters/mail-adapter';
+import { FeedbacksRepository } from '../repositories/feedbacks-repository';
+
+interface SubmitFeedbackRequest {
+  type: string;
+  comment: string;
+  screenshot?: string;
+}
+
+export class SubmitFeedback {
+  constructor(
+    private feedbacksRepository: FeedbacksRepository,
+    private mailAdapter: MailAdapter
+  ) {}
+
+  async execute(request: SubmitFeedbackRequest) {
+    const { type, comment, screenshot } = request;
+
+    await this.feedbacksRepository.create({
+      type,
+      comment,
+      screenshot,
+    });
+
+    await this.mailAdapter.sendMail({
+      subject: 'Novo feedback',
+      body: [
+        '<div style="font-fanily: sans-serif; font-size: 16px; color: #111;">',
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário do feedback: ${comment}</p>`,
+        '</div>',
+      ].join('\n'),
+    });
+  }
+}
